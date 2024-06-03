a3fb4c2a088d21efd0ac59bc5f519d4d878901cb
akira2552007 <152418934+akira2552007@users.noreply.github.com>
2024-06-03 16:33:37 +0000

    Add files via upload
    so i just started learning c and making little programs

---
+++ area_of_circle.c
@@ -0,0 +1,11 @@
+#include <stdio.h>
+
+int main() {
+    int radius;
+   
+     printf("what is the radius of the circle \n ");
+     scanf("%d" , &radius);
+float area=radius*radius*3.14;
+     printf(" the area of circle is %f" , area);
+    return 0;
+}
